9487b72f0ca24b90ade09bf6589bd4a5b38fef74
lozo6 <99729303+lozo6@users.noreply.github.com>
2022-10-24 13:27:02 -0400

    big change

--- sql_dummy_data.py
+++ sql_dummy_data.py
@@ -7,3 +7,33 @@ import os
 from faker import Faker
 import uuid
 import random
+
+# stored sensitive information in a .env file
+AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
+AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USERNAME")
+AZURE_MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
+AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
+
+connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
+db_azure = create_engine(connection_string_azure)
+
+print(db_azure.table_names())
+
+fake = Faker()
+
+fake_patients = [
+    {
+        #keep just the first 8 characters of the uuid
+        'mrn': str(uuid.uuid4())[:8], 
+        'first_name':fake.first_name(), 
+        'last_name':fake.last_name(),
+        'zip_code':fake.zipcode(),
+        'dob':(fake.date_between(start_date='-90y', end_date='-20y')).strftime("%Y-%m-%d"),
+        'gender': fake.random_element(elements=('M', 'F')),
+        'contact_mobile':fake.phone_number(),
+        'contact_home':fake.phone_number()
+    } for x in range(50)]
+
+df_fake_patients = pd.DataFrame(fake_patients)
+
+df_fake_patients = df_fake_patients.drop_duplicates(subset=['mrn'])
